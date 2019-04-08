62130ff9c6e16fec81cf06489a40aa3fd1732cf0
Dan Bent <daanikus@gmail.com>
2019-04-08 15:54:51 +1200

    Add test cases

--- main.go
+++ main.go
@@ -5,9 +5,27 @@ import (
 )
 
 func main() {
-	Export()
+	msg := "Exported"
+	Export(msg)
+	sl := []int{2, 3, 4, 5, 6, 7}
+	_ = GetSubSlice(1, sl)
+	Do()
 }
 
-func Export() {
+func Export(msg string) error {
 	fmt.Println("Exported")
+	fmt.Println(msg)
+	return nil
+}
+
+func GetSubSlice(start int, slice []int) []int {
+	return slice[start:len(slice)]
+}
+
+func Do() error {
+	err := Export("Hello")
+	if err != nil {
+		return err
+	}
+	return nil
 }
